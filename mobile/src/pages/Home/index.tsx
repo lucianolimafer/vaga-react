8c4b9bdaa27eff8d347d4131cb4e9aa85e0ddd63
lucianolimafer <lucianolimaf3r@gmail.com>
2021-02-16 12:28:17 -0300

    :rocket: Custom and finish home screen

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import {useNavigation} from '@react-navigation/native';
 
-import { Text } from 'react-native';
+import imgChair from '../../assets/images/chair.png';
+import Arrow from '../../assets/icons/arrow-right.png';
 
-import { Container } from './styles';
+import * as S from './styles';
 
 const Home: React.FC = () => {
+  const {navigate} = useNavigation();
+
+  const navigateToStore = useCallback(() => {
+    navigate('ListStore');
+  }, []);
+
   return (
-    <Container>
-      <Text>Home</Text>
-    </Container>
+    <S.Container>
+      <S.bgChair source={imgChair}/>
+      <S.textNameApp>forniturefer</S.textNameApp>
+      <S.btnCircle onPress={navigateToStore}>
+        <S.Arrow source={Arrow} />
+      </S.btnCircle>
+    </S.Container>
   );
 };
 
--- mobile/src/pages/Home/styles.ts
+++ mobile/src/pages/Home/styles.ts
@@ -1,5 +1,37 @@
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
-
+  flex: 1;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
 `;
+
+export const bgChair = styled.Image`
+  width: 250px;
+  margin-right: -100px;
+`;
+
+export const containerIntro = styled.View`
+  height: 100%;
+`;
+
+export const textNameApp = styled.Text`
+  font-size: 64px;
+  transform: rotate(-90deg);
+`;
+
+export const btnCircle = styled.TouchableOpacity`
+  width: 48px;
+  height: 48px;
+  background: #ff334a;
+  position: absolute;
+  bottom: 24px;
+  right: 24px;
+  border-radius: 50;
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+`;
+
+export const Arrow = styled.Image``;
