b0e1106e6b2b28401d003289d9d38a6759a763b5
lucianolimafer <lucianolimaf3r@gmail.com>
2021-02-16 12:29:11 -0300

    :rocket: Create first elements on the store screen

---
+++ mobile/src/pages/ListStore/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import * as S from './styles';
+
+const ListStore: React.FC = () => {
+  return (
+    <S.Container>
+    </S.Container>
+  );
+};
+
+export default ListStore;
