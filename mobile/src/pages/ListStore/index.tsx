bfb4a54fa3157e31f2bfd27d28c804dadb95cecd
lucianolimafer <lucianolimaf3r@gmail.com>
2021-02-16 16:31:39 -0300

    :rocket: Add customs and consume from fake api

--- mobile/src/pages/ListStore/index.tsx
+++ mobile/src/pages/ListStore/index.tsx
@@ -1,9 +1,143 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import SearchInput from '../../components/SearchInput';
+
+import AvatarImg from '../../assets/images/avatar.png';
+import CartIcon from '../../assets/icons/bag.png';
+
 import * as S from './styles';
+import api from '../../services/api';
+import formatValue from '../../utils/formatValue';
+import { Image } from 'react-native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+
+interface Product {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  image_url: string;
+  tumbnail_url: string;
+  formattedPrice: string;
+}
+
+interface Category {
+  id: number;
+  title: string;
+  image_url: string;
+}
 
 const ListStore: React.FC = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [categories, setCategories] = useState<Category[]>([]);
+  const [selectedCategory, setSelectedCategory] = useState<number | undefined>();
+  const [searchValue, setSearchValue] = useState('');
+
+  const {navigate} = useNavigation();
+
+  useEffect(() => {
+    async function loadProducts(): Promise<void> {
+      const response = await api.get('/products', {
+        params: {
+          category_like: selectedCategory,
+          name_like: searchValue,
+        }
+      });
+
+      setProducts(
+        response.data.map((product: Product) => ({
+          ...product,
+          formattedPrice: formatValue(product.price),
+        })),
+      );
+    }
+
+    loadProducts();
+  }, [selectedCategory, searchValue]);
+
+  useEffect(() => {
+    async function loadCategories(): Promise<void> {
+      const response = await api.get('/categories');
+
+      setCategories(response.data);
+    }
+
+    loadCategories();
+  }, []);
+
+  function handleSelectCategory(id: number): void {
+    if (selectedCategory === id) {
+      setSelectedCategory(undefined);
+    } else {
+      setSelectedCategory(id);
+    }
+  }
+
+  async function handleNavigate(id: number): Promise<void> {
+    navigate('ProducDatails', {
+      id,
+    });
+  }
+
   return (
     <S.Container>
+      <S.Header>
+        <S.textAreaHeader>
+          <S.textIntro>Melhores móveis</S.textIntro>
+          <S.textDescriptIntro>Escolha perfeita</S.textDescriptIntro>
+        </S.textAreaHeader>
+        <S.Avatar source={AvatarImg} />
+      </S.Header>
+      <S.scrollContent>
+        <SearchInput value={searchValue} onChangeText={setSearchValue} placeholder="Qual mobília você procura?" />
+        <S.categoryContainer>
+          <S.categorySlider
+            contentContainerStyle={{
+              paddingHorizontal: 20,
+            }}
+            horizontal
+            showsHorizontalScrollIndicator={false}
+          >
+            {categories.map(category => (
+              <S.categoryItem
+                key={category.id}
+                isSelected={category.id === selectedCategory}
+                onPress={() => handleSelectCategory(category.id)}
+                activeOpacity={0.6}
+                testID={`category-${category.id}`}
+              >
+                <S.categoryItemTitle
+                  isSelected={category.id === selectedCategory}
+                >
+                    {category.title}
+                </S.categoryItemTitle>
+              </S.categoryItem>
+            ))}
+          </S.categorySlider>
+        </S.categoryContainer>
+        <S.ProductsContainer>
+          <S.ProducstList>
+            {products.map(product => (
+              <S.Product
+                key={product.id}
+                testID={`product-${product.id}`}
+              >
+                <S.ProductImageContainer source={{ uri: product.image_url}} />
+                <S.ProductContent>
+                  <S.ProdtuctTitle>{product.name}</S.ProdtuctTitle>
+                  <S.ProdtuctDescription>{product.description}</S.ProdtuctDescription>
+                  <S.priceCartContent>
+                    <S.ProdtuctPrice>{product.formattedPrice}</S.ProdtuctPrice>
+                    <S.addToCartBtn onPress={() => handleNavigate(product.id)}>
+                      <S.AddText>add</S.AddText>
+                      <S.CartImage source={CartIcon} />
+                    </S.addToCartBtn>
+                  </S.priceCartContent>
+                </S.ProductContent>
+              </S.Product>
+            ))}
+          </S.ProducstList>
+        </S.ProductsContainer>
+      </S.scrollContent>
     </S.Container>
   );
 };
--- mobile/src/pages/ListStore/styles.ts
+++ mobile/src/pages/ListStore/styles.ts
@@ -1,8 +1,173 @@
-import styled from 'styled-components/native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import styled, { css } from 'styled-components/native';
+
+interface CategoryItemProps {
+  isSelected?: boolean;
+}
+
+interface CategoryItemTitleProps {
+  isSelected?: boolean;
+}
 
 export const Container = styled.View`
   flex: 1;
   display: flex;
+`;
+
+export const Header = styled.View`
+  padding: 0 24px;
+  padding-top: ${getStatusBarHeight() + 24}px;
+  width: 310px;
+  display: flex;
   flex-direction: row;
   align-items: center;
+  justify-content: space-between;
+`;
+
+export const textAreaHeader = styled.View`
+  width: 100%;
+`;
+
+export const textIntro = styled.Text`
+  font-family: 'Poppins-Medium';
+  font-size: 24px;
+  line-height: 36px;
+`;
+
+export const textDescriptIntro = styled.Text`
+  font-family: 'Poppins-ExtraLight';
+  font-size: 18px;
+  font-weight: 200;
+`;
+
+export const Avatar = styled.Image`
+  width: 48px;
+  height: 48px;
+  border-radius: 16px;
+`;
+
+export const scrollContent = styled.View`
+  width: 100%;
+  height: 100%;
+  padding: 32px 24px 0;
+`;
+
+export const categoryContainer = styled.View`
+  margin-top: 36px;
+`;
+
+export const categorySlider = styled.ScrollView`
+  width: 100%;
+`;
+
+export const categoryItem = styled.TouchableOpacity<CategoryItemProps>`
+  height: 28px;
+  align-items: center;
+  justify-content: center;
+  border-radius: 16px;
+  padding: 8px;
+  margin-right: 8px;
+  ${props =>
+    props.isSelected &&
+    css`
+      background-color: #403d54;
+      color: #fff;
+    `
+  }
+`;
+
+export const categoryItemTitle = styled.Text<CategoryItemTitleProps>`
+  font-family: 'Poppins-ExtraLight';
+  font-size: 20px;
+  font-weight: 200;
+  color: #AAA6A6;
+  ${props =>
+    props.isSelected &&
+    css`
+      color: #fff;
+    `
+  }
+`;
+
+export const ProductsContainer = styled.View`
+  margin-top: 36px;
+`;
+
+export const ProducstList = styled.View``;
+
+export const Product = styled.View`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  background: #fff;
+  padding: 8px;
+  height: 160px;
+  border-radius: 24px;
+  margin-bottom: 18px;
+`;
+
+export const ProductImageContainer = styled.Image`
+  border-radius: 24px;
+  width: 140px;
+  height: 140px;
+`;
+
+export const ProductContent = styled.View`
+  flex: 1;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  padding: 8px;
+`;
+
+export const ProdtuctTitle = styled.Text`
+  font-family: 'Poppins-Medium';
+  font-size: 20px;
+  line-height: 22px;
+  color: #403C56;
+`;
+
+export const ProdtuctDescription = styled.Text`
+  font-family: 'Poppins-Regular';
+  font-size: 10px;
+  line-height: 16px;
+  color: #403C56;
+`;
+
+export const priceCartContent = styled.View`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const ProdtuctPrice = styled.Text`
+  font-family: 'Poppins-Medium';
+  font-size: 12px;
+  color: #403C56;
+`;
+
+export const addToCartBtn = styled.TouchableOpacity`
+  width: 48px;
+  height: 20px;
+  border-radius: 8px;
+  padding: 0 4px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  background: #403C56;
+`;
+
+export const AddText = styled.Text`
+  font-family: 'Poppins-Medium';
+  font-size: 8px;
+  color: #fff;
+`;
+
+
+export const CartImage = styled.Image`
+  width: 16px;
+  height: 16px;
 `;
