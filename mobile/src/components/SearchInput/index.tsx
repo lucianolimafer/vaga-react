1f26e7267cc5cf05ca456efa0da8020f8f8b3087
lucianolimafer <lucianolimaf3r@gmail.com>
2021-02-16 13:03:59 -0300

    :hammer: Create search component

---
+++ mobile/src/components/SearchInput/index.tsx
@@ -0,0 +1,42 @@
+import React, { useCallback, useState } from 'react';
+import {TextInputProps} from 'react-native';
+
+import * as S from './styles';
+
+interface InputProps extends TextInputProps{
+  name?: string;
+}
+
+const SearchInput: React.FC<InputProps> = ({ value = '', ...rest }) => {
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false);
+    setIsFilled(!!value);
+  }, [value]);
+
+  return (
+    <S.Container isFocused={isFocused}>
+      <S.Icon
+        name="search"
+        size={24}
+        color={isFocused || isFilled ? '#403d54' : '#AAA6A6'}
+      />
+      <S.TextInput
+        placeholderTextColor="#AAA6A6"
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
+        value={value}
+        testID="search-input"
+        {...rest}
+      />
+    </S.Container>
+  );
+};
+
+export default SearchInput;
---
+++ mobile/src/components/SearchInput/styles.ts
@@ -0,0 +1,34 @@
+import styled, {css} from 'styled-components/native';
+import FeatherIcon from 'react-native-vector-icons/Feather';
+
+interface ContainerProps {
+  isFocused: boolean;
+}
+
+export const Container = styled.View<ContainerProps>`
+  width: 100%;
+  height: 60px;
+  padding: 0 16px;
+  background: #fff;
+  border-radius: 16px;
+  border-width: 2px;
+  border-color: #f0f0f5;
+  flex-direction: row;
+  align-items: center;
+  ${props =>
+    props.isFocused &&
+    css`
+      border-color: #403d54;
+    `}
+`;
+
+export const TextInput = styled.TextInput`
+  flex: 1;
+  color: #403d54;
+  font-size: 16px;
+  font-family: 'Poppins-Regular';
+`;
+
+export const Icon = styled(FeatherIcon)`
+  margin-right: 16px;
+`;
